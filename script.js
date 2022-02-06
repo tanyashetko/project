7dda0ab7988d12e8123575b3c69ca1818caa6e8a
Tanya <tanya@email.ru>
2022-02-06 19:43:18 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// console.log(1);
+// let number = 5;
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     name: 'Dimok',
+//     age: 30,
+//     isMan: true
+// };
+// // console.log(obj.age);
+
+// alert('Hello, Dimok');
+// const result = confirm('Are you here?');
+const answer = prompt('Тебе есть 18?', '');
+console.log(answer);
