7bb2569f450956179675d39757885d733afb86e8
Tanya <tanya@email.ru>
2022-02-06 23:49:32 +0300

    add script file

--- script.js
+++ script.js
@@ -13,5 +13,22 @@
 
 // alert('Hello, Dimok');
 // const result = confirm('Are you here?');
-const answer = prompt('Тебе есть 18?', '');
-console.log(answer);
+// const answer = prompt('Тебе есть 18?', '');
+// console.log(answer);
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','');
+console.log(numberOfFilms);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('Один из последних просмотренных фильмов'),
+      b = prompt('На сколько оцените его?'),
+      c = prompt('Один из последних просмотренных фильмов'),
+      d = prompt('На сколько оцените его?');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
